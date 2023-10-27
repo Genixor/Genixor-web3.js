69685f48179558383fafd2b87350b839f48d4b65
Loris Leiva <loris.leiva@gmail.com>
2023-10-27 13:50:10 +0100

    refactor(experimental): parameterize account metas for instructions and instructions for transactions

--- packages/transactions/src/types.ts
+++ packages/transactions/src/types.ts
@@ -4,23 +4,27 @@ import { IAccountMeta, IInstruction } from '@solana/instructions';
 export type SerializedMessageBytes = Uint8Array & { readonly __serializedMessageBytes: unique symbol };
 export type SerializedMessageBytesBase64 = string & { readonly __serializedMessageBytesBase64: unique symbol };
 
-export type BaseTransaction = Readonly<{
-    instructions: readonly IInstruction[];
-    version: TransactionVersion;
+export type BaseTransaction<
+    TVersion extends TransactionVersion = TransactionVersion,
+    TInstruction extends IInstruction = IInstruction
+> = Readonly<{
+    instructions: readonly TInstruction[];
+    version: TVersion;
 }>;
 
-type LegacyTransaction = BaseTransaction &
-    Readonly<{
-        instructions: readonly (Omit<IInstruction, 'accounts'> &
-            Readonly<{ readonly accounts?: readonly IAccountMeta[] }>)[];
-        version: 'legacy';
-    }>;
+type ILegacyInstruction<TProgramAddress extends string = string> = IInstruction<
+    TProgramAddress,
+    readonly IAccountMeta[]
+>;
+type LegacyTransaction<TInstruction extends ILegacyInstruction = ILegacyInstruction> = BaseTransaction<
+    'legacy',
+    TInstruction
+>;
 
-type V0Transaction = BaseTransaction &
-    Readonly<{
-        instructions: readonly IInstruction[];
-        version: 0;
-    }>;
+type V0Transaction<TInstruction extends IInstruction = IInstruction> = BaseTransaction<0, TInstruction>;
 
-export type Transaction = LegacyTransaction | V0Transaction;
+export type Transaction<TInstruction extends IInstruction = IInstruction> =
+    | LegacyTransaction<TInstruction>
+    | V0Transaction<TInstruction>;
+
 export type TransactionVersion = 'legacy' | 0;
