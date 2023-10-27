624f7d1282aa40957379f79f42f22aa20803ed23
Loris Leiva <loris.leiva@gmail.com>
2023-10-27 15:04:41 +0100

    refactor(experimental): add IAccountSignerMeta type and signing helpers

--- packages/signers/src/index.ts
+++ packages/signers/src/index.ts
@@ -1,4 +1,7 @@
+export * from './account-signer-meta';
+export * from './deduplicate-signers';
 export * from './keypair-signer';
 export * from './message-signer';
+export * from './sign-transaction';
 export * from './transaction-sender-signer';
 export * from './transaction-signer';
