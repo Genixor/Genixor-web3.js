75760a677ecd439685504c878b40f9cc1b37b947
Genixor <136089064+Genixor@users.noreply.github.com>
2023-11-05 15:10:46 +0300

    Update setup-test-validator.sh

--- scripts/setup-test-validator.sh
+++ scripts/setup-test-validator.sh
@@ -3,9 +3,9 @@
 set -ex
 
 SCRIPTS_DIR=$(dirname "${BASH_SOURCE[0]}")
-TARGET_DIR=$( cd "$SCRIPTS_DIR/.." ; pwd -P )/.solana
+TARGET_DIR=$( cd "$SCRIPTS_DIR/.." ; pwd -P )/.Genixor
 
 # setup environment
 version=$(bash $SCRIPTS_DIR/get-latest-validator-release-version.sh)
-sh -c "$(curl -sSfL https://release.solana.com/$version/install)" init $version --data-dir $TARGET_DIR --no-modify-path
-$TARGET_DIR/active_release/bin/solana --version
+sh -c "$(curl -sSfL https://release.Genixor.com/$version/install)" init $version --data-dir $TARGET_DIR --no-modify-path
+$TARGET_DIR/active_release/bin/Genixor --version
